4cd7c035d41e60238f0ed2b85729db4f7370da99
Moolabs <155178710+MooLabsCan@users.noreply.github.com>
2024-06-06 15:35:12 +0400

    Add files via upload

--- RotateGame/script.js
+++ RotateGame/script.js
@@ -1,14 +1,30 @@
 
     let previousOrientation = window.orientation;
     const display = document.getElementById('do');
-function checkOrientation (){
-    if(window.orientation !== previousOrientation){
+    const vis = document.getElementById('vis');
+
+
+    window.document.addEventListener('load', checkOrientation)
+
+    function detectDevice() {
+        return window.matchMedia("(hover: none), (pointer: coarse)").matches;
+    }
+    function checkOrientation (){
+    if(window.orientation !== '90'){
         previousOrientation = window.orientation;
         console.log(previousOrientation);
         display.innerHTML = previousOrientation;
+       if (previousOrientation === 90) vis.src = 'ls.jpeg';
+       else if (previousOrientation === 0)   vis.src = 'Rotatescreen.gif';
 
     }
 }
 
 window.addEventListener("resize", checkOrientation, false);
 window.addEventListener("orientationchange", checkOrientation, false);
+
+if(detectDevice()) setInterval(checkOrientation, 2000)
+    else  {
+        display.innerHTML = 'desktop detected';
+    vis.src = 'ls.jpeg';
+}
