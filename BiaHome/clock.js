4fb6475f6758ae85e0046736cac5f41cccd6e0be
Moolabs <155178710+MooLabsCan@users.noreply.github.com>
2024-06-12 11:56:03 +0400

    Add files via upload

---
+++ BiaHome/clock.js
@@ -0,0 +1,30 @@
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+
+
+
+const days = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+const months = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
+
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth()
+    const day = time.getDay()
+    const date = time.getDate()
+    const hours = time.getHours()
+    const hoursForClock = hours % 12
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
+
+    timeEl.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
+    dateEl.innerHTML =`${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime()
+
+setInterval(setTime, 1000)
